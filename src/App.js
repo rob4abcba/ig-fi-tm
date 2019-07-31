61784bd5d3ff7df4d5210ae6b072e5b77dcd9fa6
rob4abcba <rob_abcba@yahoo.com>
2019-07-30 19:31:37 -0700

    Demo passing props vs use state. componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Header";
 
 class App extends Component {
   state = {
     count: 10
-  };
+  }; 
+  componentDidMount = () => {
+    console.log("App.js > Component really did mount")
+    this.plus()
+  }
   plus = () => {
     console.log("Plus");
     let num = this.state.count + 1;
@@ -17,11 +20,10 @@ class App extends Component {
     let num = this.state.count - 1;
     this.setState({ count: num });
   };
-
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header countRL={this.state.count}/>
         <p>Count = {this.state.count}</p>
         <p>Click any of the buttons below.</p>
         <button onClick={() => this.plus()}> Plus </button>
--- src/Header.js
+++ src/Header.js
@@ -3,11 +3,18 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class Header extends Component {
+    state = {
+        countRL: 5
+    }
+    componentDidMount = () => {
+        console.log("Header.js > Component really did mount")
+      }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.props.countRL}</p>
         </header>
       </div>
     );
