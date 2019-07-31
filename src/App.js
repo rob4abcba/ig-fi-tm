bc45717e6574b7bad17b9709621cda7ba15cbd47
rob4abcba <rob_abcba@yahoo.com>
2019-07-30 18:35:35 -0700

    Separate Header.js away from toplevel App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
 
 class App extends Component {
   state = {
     count: 10
-  }
+  };
   plus = () => {
-    console.log("Plus")
-    let num = this.state.count + 1
-    this.setState({count: num})
-  }
+    console.log("Plus");
+    let num = this.state.count + 1;
+    this.setState({ count: num });
+  };
   minus = () => {
-    console.log("Minus")
-    let num = this.state.count - 1
-    this.setState({count: num})
-  }
+    console.log("Minus");
+    let num = this.state.count - 1;
+    this.setState({ count: num });
+  };
 
   render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Count = {this.state.count}
-          </p>
-        <p>
-          Click any of the buttons below.
-        </p>
+    return (
+      <div className="App">
+        <Header />
+        <p>Count = {this.state.count}</p>
+        <p>Click any of the buttons below.</p>
         <button onClick={() => this.plus()}> Plus </button>
         <button onClick={() => this.minus()}> Minus </button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      </div>
+    );
+  }
 }
 
 export default App;
